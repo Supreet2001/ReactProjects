ecf0643685e4447fe32a3c565b13571ed49455f4
Supreet Parida <Supreet.Parida@Ltts.com>
2026-04-17 14:40:08 +0530

    Add Players Name & Edit Functionality

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,9 +1,15 @@
 
+import Player from "./components/Player"
 function App() {
-  
-
   return (
-    <p>Coming soon...</p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player player="Player 1" symbol ="X"/>
+          <Player player="Player 2" symbol ="O"/>
+        </ol>
+      </div>
+    </main>
   )
 }
 
---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+export default function Player({ player, symbol }) {
+    const [isEditing, setIsEditing] = useState(false)
+    const[name, setName] = useState(player)
+    let content = <span className="player-name">{name}</span>
+
+    function handleBtnClick() {
+        // setIsEditing(!isEditing) => schedule a future update
+        // if the state value depends on old state, never update like above, rather use function to update
+
+        setIsEditing(item=>!item) // this is the correct way
+
+    }
+
+    function handleChange(event){
+        setName(event.target.value) //two-way-binding
+    }
+
+    if (isEditing) {
+        content = <input type="text" required value={name} onChange={handleChange}/>
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {content}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleBtnClick}>{isEditing?"Save":"Edit"}</button>
+        </li>
+    )
+}

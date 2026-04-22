7b4af78c886a8f6688ff1a99ed4ad46b093e6990
Supreet Parida <supreetparida2001@outlook.com>
2026-04-22 22:29:04 +0530

    Fix validation for styled components

--- 01-starting-project-css/01-starting-project/src/components/Header.jsx
+++ 01-starting-project-css/01-starting-project/src/components/Header.jsx
@@ -1,11 +1,11 @@
 import logo from '../assets/logo.png';
-
+import StyledHeader from './StyledHeader';
 export default function Header() {
   return (
-    <header>
+    <StyledHeader>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
-    </header>
+    </StyledHeader>
   );
 }
